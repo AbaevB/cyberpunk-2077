575047847571eee0b73b625cbd1728a58b336123
AbaevB <abaev70@hotmail.com>
2024-06-05 23:37:04 +0700

    feat(hero): add slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
- 
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+
+    autoplay: {
+      delay: 3000,
+    },
+
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+
+    controller: {
+      inverse: true,
+    },
+
+  });
+
+
+});
+
